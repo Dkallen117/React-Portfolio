f4701da5de91ec146e17a624c888338d7745db39
Daniel Allen <danielkentonallen@gmail.com>
2021-10-13 15:23:02 -0600

    added message form in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,19 +3,48 @@ import '../assets/css/style.css'
 export default class Footer extends Component {
   render() {
    return (
- <footer className ="row justify-content-center my-4 animated fadeInUp" id ="contact">
+<footer>
+    <div className="mask d-flex align-items-center">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-12 col-md-10 col-lg-7 col-xl-6">
+                  <form className="card bg-image gradient-custom mask-custom p-5 text-white animated fadeInUp">
+                  <h2 className="text-center mb-5">Message Form</h2>
+                  <div className="form-outline form-white mb-4">
+                      <input type="text" id="form3Example4" className="form-control form-control-lg" />
+                      <label className="form-label" for="form3Example4">Name</label>
+                    </div>
 
-    <div className ="col-12 col-sm-6 col-md-4">
-      <address>
+                    <div className="form-outline form-white mb-4">
+                      <input type="email" id="form3Example3" className="form-control form-control-lg" />
+                      <label className="form-label" for="form3Example3">Email address</label>
+                    </div>
 
-        <h5>
-          Email me at: <a href="mailto:danielkentonallen@gmail.com">danielkentonallen@gmail.com </a>
-        </h5>
+                    <div className="form-outline form-white mb-4">
+                    <textarea className="form-control" id="form4Example3" rows="4"></textarea>
+                    <label className="form-label" for="form4Example3">Message</label>
+                    </div>
 
-      </address>
+                    <div className="form-check d-flex  mb-4">
+                      <input
+                        className="form-check-input me-2"
+                        type="checkbox"
+                        value=""
+                        id="form2Example3"
+                        checked
+                      />
+                      <label className="form-check-label" for="form2Example3">
+                      Send me a copy of this message
+                      </label>
+                    </div>
+                    <button type="submit" className="btn btn-light btn-block mb-4">Send</button>
+                  </form>  
+          </div>
+        </div>
+      </div>
     </div>
-
   </footer>
+
     );
   }
 }
