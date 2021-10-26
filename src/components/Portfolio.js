b77560ad75c5f0cb19108f2c77662138cc7ebd16
Daniel Allen <danielkentonallen@gmail.com>
2021-10-26 17:32:51 -0600

    cleaned up portfolio section, made mobile responsive

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -10,20 +10,22 @@ export default class Porfolio extends Component {
   render() { 
     return (
 
-<section className="container"   id="main-Section">
-
+<section className="container-fluid"   id="main-Section">
 {/* Abilities and Interests */}
-<h1 className = "text-center my-5">About</h1>
-<h2  className ="text-center my-5"> Abilities and Interests </h2>
-  <section className="d-flex justify-content-around ml-4" id="abilities-Interests">
-
+<h1 className = "wow fadeInUp text-center my-5">About</h1>
+<hr className="wow fadeInUp mb-5" style={{borderWidth: '10px', backgroundColor:  '#00000099', color:'#FFFFFF',}}></hr>
+<h2  className ="wow fadeInUp text-center my-5"> Abilities and Interests </h2>
+  <section className="container-fluid ml-3" id="abilities-Interests">
+    <div className="row d-flex justify-content-around ">
+    
+    
     <div className="shape d-flex flex-column justify-content-center align-items-center wow fadeInUp">
     <i className="fas fa-database fa-2x"></i>
     <h2>Back End</h2>
     <p className="text-center"> Database Architecture and MySQL</p>
     </div>
 
-    <div className="shape d-flex flex-column justify-content-center align-items-center  wow fadeInUp">
+    <div className="shape d-flex flex-column justify-content-center align-items-center wow fadeInUp">
     <i className="fab fa-bootstrap fa-2x"></i>
     <h2>Front End</h2>
     <p className="text-center"> Creatively Designed, Intuitively Driven </p>
@@ -40,11 +42,11 @@ export default class Porfolio extends Component {
     <h2>Source Control</h2>
     <p className="text-center"> Scrum Master and Team Player </p>
     </div>
-  
+    </div>
   </section>
 {/* Skills */}
   <div className = "container my-5"> 
-    <h1 className = "text-center my-5 wow zoomIn"> Skill Set Knowledge </h1>
+    <h2 className = "text-center my-5 wow zoomIn"> Skill Set Knowledge </h2>
     <div className="row d-flex justify-content-around">
       
       <div className="col-md-3 col-sm-6 wow fadeInLeft">
@@ -154,35 +156,28 @@ export default class Porfolio extends Component {
   </div> 
   
   <h1 className="wow slideInLeft my-4 text-center" id="work-Title">Portfolio</h1>
-       {/* Stock Scanner Card */}     
-    <div className="row row-cols-1 row-cols-md-2">
-      <div className="col-md-4 col-sm-12 mb-4">  
-        <div className="card wow fadeInUp text-center">
-                          
+  <hr className="wow slideInLeft mb-5" style={{borderWidth: '10px', backgroundColor:  '#00000099', color:'#FFFFFF',}}></hr>
+       {/* Stock Scanner Card */}          
+    <div className="card-group row row-cols-1 row-cols-md-2">
+      <div className="col-md-4 mb-4 my-5">  
+        <div className="card d-flex flex-column wow fadeInUp text-center">                     
           <div className="bg-image hover zoom">
           <a href="https://dkallen117.github.io/StockScanner/" target="_blank" rel="noreferrer">
           <img className="card-img-top img-fluid" id="stock-Scanner-Photo" src = {stockScanner} alt="Stock Scanner"></img>
           </a>     
           </div>
-          <div className="mask rgba-white-slight"></div>
-          
-                        
-          <div className="card-body">
-                     
+          <div className="mask rgba-white-slight"></div>        
            <h4 className="card-title">Stock Scanner</h4>               
             <p className="card-text">Click the image to be taken to my first team-based website
-                                 utilizing two different APIs.</p>
+                                 utilizing two different APIs. </p>  
             <a href="https://github.com/Dkallen117/StockScanner" target="_blank" rel="noreferrer">
-            <button type="button" className="btn blue-gradient btn-lg">GitHub Respository</button>
+            <button type="button" className="btn blue-gradient btn-lg mb-5">GitHub Respository</button>
             </a>
-                
-          </div>
         </div>        
-      </div>
-          
+      </div>        
        {/* Snarf Card */}
-      <div className="col-md-4 col-sm-12 mb-4"> 
-        <div className="card wow fadeInUp text-center">         
+      <div className="col-md-4 mb-4 my-5"> 
+        <div className="card d-flex flex-column wow fadeInUp text-center" id="snarf-Card">         
           <div className="bg-image hover zoom">
           <a href="https://prezi.com/dwii7bnwk3rz/team-everybody-loves-snarf/" target="_blank" rel="noreferrer">
           <img className="card-img-top" src = {snarfPic}
@@ -191,89 +186,61 @@ export default class Porfolio extends Component {
           <div className="mask rgba-white-slight"></div>
           </a>
           </div>
-          <div className="card-body">
-      
-           <h4 className="card-title">Video Game Studio Prezi Presentation</h4>
-                  
+           <h4 className="card-title mt-5">Video Game Studio Prezi Presentation</h4> 
             <p className="card-text">Click the image to be taken to a team-based prezi presentation 
             for a fictional video game studio! (Image Credit: Jason Velazquez // 
-            <a href="https://dribbble.com/shots/1453837-Snarf-minimal-vector-drawing-ThunderCats" target="_blank" rel="noreferrer">Image Link</a>)</p>
-                
-           </div>              
+            <a href="https://dribbble.com/shots/1453837-Snarf-minimal-vector-drawing-ThunderCats" target="_blank" rel="noreferrer">Image Link</a>)</p>            
        </div>  
-      </div>
-      
+      </div> 
         {/* Metatron Card */}
-      <div className="col-md-4 col-sm-12 mb-4">            
-        <div className="card wow fadeInUp text-center">
-          
+      <div className="col-md-4 mb-4 my-5">            
+        <div className="card d-flex flex-column wow fadeInUp text-center">
          <div className="bg-image hover zoom">
           <a href="https://www.youtube.com/watch?v=WM8Mky99coE" target="_blank" rel="noreferrer">
-          <img className="card-img-top" id="meta-Cube-Photo" src = {metatronPic} alt="Metatrons Cube">
-              
-          </img>
+          <img className="card-img-top" id="meta-Cube-Photo" src = {metatronPic} alt="Metatrons Cube"></img>
           <div className="mask rgba-white-slight"></div>
           </a>
           </div>  
-          <div className="card-body">
-          
-           <h4 className="card-title">Metatron's Cube YouTube Video</h4>
-                 
+           <h4 className="card-title mt-5">Metatron's Cube YouTube Video</h4>        
             <p className="card-text">Click the image to be taken to a YouTube video of my teams fictional video game
-                                 pitch to a fictional game studio.</p>
-                  
-          </div>           
-       </div>     
-      </div>
+                                     pitch to a fictional game studio.</p>             
+        </div>     
+       </div>
       
        {/* Weather Card */}
-      <div className="col-md-4 col-sm-12 mb-4" id="weather-Card">        
-         <div className="card wow fadeInUp text-center">
-          
+        <div className="col-md-4 mb-4 my-5" id="weather-Card">        
+         <div className="card d-flex flex-column wow fadeInUp text-center">
           <div className="bg-image hover zoom">
            <img className="card-img-top img-fluid" id="code-Refractor-Photo" src = {weatherPic} alt="Weather"></img>   
           </div>
           <a href="#!">
           <div className="mask rgba-white-slight"></div>
-          </a>  
-            <div className="card-body">
-             <h4 className="card-title">Weather Dashboard</h4>                 
-               <p className="card-text">Click the image to be taken to a daily and 5-day forecast Weather Dashboard 
-                                    application.
-               </p>
-
-               <a href="https://github.com/Dkallen117/weather-Dashboard" target="_blank" rel="noreferrer">
-               <button type="button" className="btn blue-gradient btn-lg">GitHub Respository</button>
-               </a>
-          
-            </div>
-           </div>
+          </a>           
+            <h4 className="card-title">Weather Dashboard</h4>                 
+              <p className="card-text">Click the image to be taken to a daily and 5-day forecast weather application. </p>
+              <a href="https://github.com/Dkallen117/weather-Dashboard" target="_blank" rel="noreferrer">
+              <button type="button" className="btn blue-gradient btn-lg mb-5">GitHub Respository</button>
+              </a>
+          </div>
         </div>
 
         {/* Code Refactor Card */}
-        <div className="col-md-4 col-sm-12 mb-4"  id="code-Card">                   
-          <div className="card wow fadeInUp text-center">
+        <div className="col-md-4 mb-4 my-5"  id="code-Card">                   
+          <div className="card d-flex flex-column wow fadeInUp text-center">
            <div className="bg-image hover zoom">
            <img className="card-img-top img-fluid" id="code-Refractor-Photo" src = {refactorPic} alt="Code Refractor"></img>
            </div>
            <a href="#!">
            <div className="mask rgba-white-slight"></div>
            </a>
-
-            <div className="card-body">
-
-             <h4 className="card-title">Code Refracting</h4>
-                  
+             <h4 className="card-title">Code Refracting</h4>                
               <p className="card-text">Click the image to be taken to the first website I practiced code refractoring.</p>
-
                <a href="https://github.com/Dkallen117/code-Refractor" target="_blank" rel="noreferrer">
-               <button type="button" className="btn blue-gradient btn-lg">GitHub Respository</button>
-               </a>
-          
-            </div>          
+               <button type="button" className="btn blue-gradient btn-lg mb-5">GitHub Respository</button>
+               </a>                
            </div>         
           </div>         
-      </div>        
+    </div>        
    </section>
         );
   }
