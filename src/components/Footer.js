57e5b1e0681041e207b7b3d5feff7e86cb95e671
Daniel Allen <danielkentonallen@gmail.com>
2021-12-29 18:21:52 -0700

    added reset input fields for contact form

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import emailjs, { init } from 'emailjs-com';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -6,7 +6,20 @@ import '../assets/css/style.css'
 function Footer() {
   init("user_MtnRDz54CbneHj0t1h6cd");
   const notify = () => toast("Wow so easy!");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
   const form = useRef();
+
+  const handleChange = (event) => {
+    event.target.name === "name"
+      ? setName(event.target.value)
+      : event.target.name === "email"
+        ? setEmail(event.target.value)
+        : event.target.name === "message"
+          ? setMessage(event.target.value)
+          : console.log(event.error)
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
@@ -14,6 +27,10 @@ function Footer() {
     emailjs.sendForm('service_qq9qhxe', 'template_5y6thzi', form.current)
       .then((result) => {
         console.log(result.text);
+        notify();
+        setName("");
+        setEmail("");
+        setMessage("");
       }, (error) => {
         console.log(error.text);
       });
@@ -28,20 +45,20 @@ function Footer() {
               <form className="card bg-image gradient-custom mask-custom p-5 text-white wow fadeInUp" ref={form} onSubmit={sendEmail}>
                 <h2 className="text-center mb-5">Message Form</h2>
                 <div className="form-outline form-white mb-4">
-                  <input type="text" name="user_name" id="name" className="form-control form-control-lg" required />
+                  <input type="text" name="name" id="name" value={name} onChange={(e) => handleChange(e)} className="form-control form-control-lg" required />
                   <label className="form-label" for="form3Example4">Name</label>
                 </div>
 
                 <div className="form-outline form-white mb-4">
-                  <input type="email" name="user_email" id="email" className="form-control form-control-lg" required />
+                  <input type="email" name="email" id="email" value={email} onChange={(e) => handleChange(e)} className="form-control form-control-lg" required />
                   <label className="form-label" for="form3Example3">Email address</label>
                 </div>
 
                 <div className="form-outline form-white mb-4">
-                  <textarea className="form-control" name="message" id="message" rows="4" required></textarea>
+                  <textarea className="form-control" name="message" id="message" value={message} onChange={(e) => handleChange(e)} rows="4" required></textarea>
                   <label className="form-label" for="form4Example3">Message</label>
                 </div>
-                <button type="submit" onClick={notify} value="Send" className="btn btn-light btn-block mb-4">Send</button>
+                <button type="submit" value="Send" className="btn btn-light btn-block mb-4">Send</button>
                 <ToastContainer />
               </form>
               <div className="d-flex justify-content-around my-3">
