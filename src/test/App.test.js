c96eaeb0e9abad4cd7a719c8ef2acd919068e0c6
Daniel Allen <danielkentonallen@gmail.com>
2022-05-17 17:35:09 -0600

    button click test working

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -1,19 +1,17 @@
 import React from "react";
-import { configure, mount, shallow } from "enzyme";
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import chai, { expect } from "chai";
 import chaiEnzyme from "chai-enzyme";
-import Adapter from "enzyme-adapter-react-16";
 import sinon from 'sinon';
 import Footer from "../components/Footer"
 
-configure({
-   adapter: new Adapter()
-});
+configure({ adapter: new Adapter() });
 
 describe("Email", () => {
    it("should have a button", () => {
       const wrapper = shallow(<Footer />);
-      expect(wrapper.find('button')).to.have.length(1);
+      expect(wrapper.find('button')).to.have.length(2);
    });
 
    it("should have two inputs, one for name, one for email", () => {
@@ -23,11 +21,11 @@ describe("Email", () => {
    });
 
    it('should simulate click events', () => {
-      const onButtonClick = sinon.spy();
-      const wrapper = shallow(<Footer onButtonClick={onButtonClick} />);
-      wrapper.find('button').simulate('click');
-      expect(onButtonClick).to.have.property('callCount', 1);
 
+      const wrapper = mount(<Footer />);
+
+      wrapper.find('button').at(0).simulate('click');
+      wrapper.find('button').at(1).simulate('click');
 
    })
 
